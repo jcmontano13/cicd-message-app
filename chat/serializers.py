1cb306de5c26cd976ae8a6b05195654853e3e890
jcmontano13 <jeck.montano@yahoo.com>
2026-03-01 17:54:15 +1300

    1. updated serializers.py removing logic 2. updated my views.py to perform registration.

--- chat/serializers.py
+++ chat/serializers.py
@@ -2,20 +2,3 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import UserProfile
-
-
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=6)
-
-    class Meta:
-        model = User
-        fields = ["username", "email", "password"]
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data["username"],
-            email=validated_data.get("email"),
-            password=validated_data["password"]
-        )
-        UserProfile.objects.create(user=user)
-        return user
